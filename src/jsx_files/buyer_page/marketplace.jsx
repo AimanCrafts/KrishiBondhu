8e8c69d697fa482111205286dd97f137eb8a4641
Abdur Rahman Aiman <192534791+AimanCrafts@users.noreply.github.com>
2026-03-28 22:54:02 +0600

    Adding JSX Files for Buyer Dashboard
    Signed-off-by: Abdur Rahman Aiman <192534791+AimanCrafts@users.noreply.github.com>

---
+++ src/jsx_files/buyer_page/marketplace.jsx
@@ -0,0 +1,19 @@
+export default function Marketplace() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Crop Marketplace</h2>
+
+      <div className="crop-card">
+        <h3>Rice</h3>
+        <p>Farmer: Rahman</p>
+        <button>Buy</button>
+      </div>
+
+      <div className="crop-card">
+        <h3>Potato</h3>
+        <p>Farmer: Karim</p>
+        <button>Buy</button>
+      </div>
+    </div>
+  );
+}
